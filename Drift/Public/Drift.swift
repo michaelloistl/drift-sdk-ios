9b032022b1252929035d7c8c692bfd8aa1f007b9
Michael Loistl <michael@aplo.io>
2017-05-11 17:30:18 +0100

    Add completion block to user registration

--- Drift/Public/Drift.swift
+++ Drift/Public/Drift.swift
@@ -10,57 +10,59 @@ import Foundation
 
 @objc
 open class Drift: NSObject {
-
-    
-    /**
-     Initialise Driftt SDK with an embed ID.
-     
-     - Parameter embedId: Embed ID found in Driftt Settings
-     
-    */
-    open class func setup(_ embedId: String) {
-        DriftManager.retrieveDataFromEmbeds(embedId)
-        DriftManager.createTemporaryDirectory()
+  
+  
+  /**
+   Initialise Driftt SDK with an embed ID.
+   
+   - Parameter embedId: Embed ID found in Driftt Settings
+   
+   */
+  open class func setup(_ embedId: String) {
+    DriftManager.retrieveDataFromEmbeds(embedId)
+    DriftManager.createTemporaryDirectory()
+  }
+  
+  /**
+   Registers Users with drift. Should be completed after user login
+   
+   - Parameter userId: The User id from your database. Will be the same as on driftt.
+   
+   */
+  open class func registerUser(_ userId: String, email: String, completion: @escaping (Bool) -> Void) {
+    DriftManager.registerUser(userId, email: email, attrs: nil) { (success) in
+      completion(success)
     }
-    
-    /**
-     Registers Users with drift. Should be completed after user login
-    
-     - Parameter userId: The User id from your database. Will be the same as on driftt.
-     
-    */
-    open class func registerUser(_ userId: String, email: String) {
-        DriftManager.registerUser(userId, email: email, attrs: nil)
-    }
-    
-    /**
-     Logs users out of Drift
-     */
-    open class func logout() {
-        LayerManager.logout()
-        DriftManager.logout()
-    }
-    
-    
-    /**
-
-     This mode enables you to see the output logs of drift for debug purposes
-     This will also stop dismissing announcements from being sticky so you can see the same announcement over and over
-     
-     - parameter debug: A Bool indicating if debug mode should be enabled or not
-     
-    */
-    open class func debugMode(_ debug:Bool) {
-        DriftManager.debugMode(debug)
-    }
-    
-    /**
-
-     This will show a list of Drift conversations for the current user
-     
-     */
-    open class func showConversations() {
-        DriftManager.showConversations()
-    }
-    
+  }
+  
+  /**
+   Logs users out of Drift
+   */
+  open class func logout() {
+    LayerManager.logout()
+    DriftManager.logout()
+  }
+  
+  
+  /**
+   
+   This mode enables you to see the output logs of drift for debug purposes
+   This will also stop dismissing announcements from being sticky so you can see the same announcement over and over
+   
+   - parameter debug: A Bool indicating if debug mode should be enabled or not
+   
+   */
+  open class func debugMode(_ debug:Bool) {
+    DriftManager.debugMode(debug)
+  }
+  
+  /**
+   
+   This will show a list of Drift conversations for the current user
+   
+   */
+  open class func showConversations() {
+    DriftManager.showConversations()
+  }
+  
 }
